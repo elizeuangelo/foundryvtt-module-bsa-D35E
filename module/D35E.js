b4582792f770f77d601352baef5bb88a02979f63
elizeuangelo <elizeu90@gmail.com>
2023-07-22 11:22:01 -0300

    rudimentary implementation

--- module/D35E.js
+++ module/D35E.js
@@ -3,7 +3,7 @@ export class D35E {
         return 2;
     }
     get id() {
-        return "D35E";
+        return 'D35E';
     }
     async actorRollSkill(actor, skillId) {
         let roll = await actor.rollSkill(skillId);
@@ -11,10 +11,6 @@ export class D35E {
     }
     async actorRollAbility(actor, abilityId) {
         let roll = await actor.rollAbilityTest(abilityId);
-        return this.fixReadySetRoll(roll);
-    }
-    async actorRollTool(actor, item) {
-        let roll = await item.rollToolCheck();
         return this.fixReadySetRoll(roll);
     }
     fixReadySetRoll(roll) {
@@ -29,70 +25,112 @@ export class D35E {
         return roll;
     }
     actorCurrenciesGet(actor) {
-        return actor["system"].currency;
+        function addCurrency(currency) {
+            if (currency)
+                Object.entries(currency).forEach(([k, v]) => (currencies[k] = (currencies[k] ?? 0) + v));
+        }
+        const currencies = {};
+        addCurrency(actor.system.currency);
+        addCurrency(actor.system.altCurrency);
+        addCurrency(actor.system.customCurrency);
+        return currencies;
     }
     async actorCurrenciesStore(actor, currencies) {
         await actor.update({ system: { currency: currencies } });
     }
     actorSheetAddTab(sheet, html, actor, tabData, tabBody) {
+        if (sheet.constructor.name !== 'Character')
+            return;
         const tabs = $(html).find('.tabs[data-group="primary"]');
         const tabItem = $('<a class="item" data-tab="' + tabData.id + '">' + tabData.label + '</a>');
         tabs.append(tabItem);
-        const body = $(html).find(".sheet-body");
+        const body = $(html).find('.primary-body');
         const tabContent = $('<div class="tab" data-group="primary" data-tab="' + tabData.id + '"></div>');
         body.append(tabContent);
         tabContent.append(tabBody);
+        if (sheet.position.width < 836) {
+            sheet.position.width = 836;
+            sheet.element[0].style.width = '836px';
+        }
+    }
+    itemSheetReplaceContent(app, html, element) {
+        html.find('.summary')[0].innerHTML = '<li>Recipe</li>';
+        if (game.user.isGM === false && app.object.system.identified === false)
+            return;
+        const sheetNavigation = html.find('.sheet-navigation[data-group=primary]');
+        const initialTab = app._initialTab.primary === 'crafting';
+        sheetNavigation.append($(`<a class="item ${initialTab ? 'active' : ''}" data-tab="crafting">Crafting</a>`));
+        const body = $(`<div class="tab crafting ${initialTab ? 'active' : ''}" data-group="primary" data-tab="crafting"></div>`);
+        body.append(element);
+        html.find('.primary-body').append(body);
     }
     get configSkills() {
-        return Object.entries(game["D35E"].config.skills)
-            .map(skills => {
-            return { id: skills[0], label: skills[1].label };
+        function includeSubSkills(actor) {
+            arbitrarySkills.forEach((id) => {
+                const skill = actor.system.skills[id];
+                if (skill === undefined)
+                    return;
+                Object.entries(skill.namedSubSkills).forEach(([k, v]) => {
+                    const subSkillId = `${id}.namedSubSkills.${k}`;
+                    if (subSkillId in skills)
+                        return;
+                    const label = `${CONFIG['D35E'].skills[id]}: ${v.name}`;
+                    skills[subSkillId] = { id: subSkillId, label };
+                });
+            });
+        }
+        const arbitrarySkills = CONFIG['D35E'].arbitrarySkills;
+        const skills = {};
+        Object.entries(CONFIG['D35E'].skills).forEach(([id, label]) => {
+            if (arbitrarySkills.includes(id) === false)
+                skills[id] = { id, label };
         });
+        game.actors.forEach(includeSubSkills);
+        return Object.values(skills).sort((a, b) => a.label.localeCompare(b.label));
     }
     get configAbilities() {
-        return Object.entries(game["D35E"].config.abilities).map(ab => {
+        return Object.entries(CONFIG['D35E'].abilities).map((ab) => {
             return { id: ab[0], label: ab[1] };
         });
     }
     get configCurrencies() {
-        return [
+        const currencies = [
             {
-                id: "pp",
+                id: 'pp',
                 factor: 1000,
-                label: game["D35E"].config.currencies.pp.label
+                label: CONFIG['D35E'].currencies.pp,
             },
             {
-                id: "gp",
+                id: 'gp',
                 factor: 100,
-                label: game["D35E"].config.currencies.gp.label
+                label: CONFIG['D35E'].currencies.gp,
             },
             {
-                id: "ep",
-                factor: 50,
-                label: game["D35E"].config.currencies.ep.label
-            },
-            {
-                id: "sp",
+                id: 'sp',
                 factor: 10,
-                label: game["D35E"].config.currencies.sp.label
+                label: CONFIG['D35E'].currencies.sp,
             },
             {
-                id: "cp",
+                id: 'cp',
                 factor: 1,
-                label: game["D35E"].config.currencies.cp.label
-            }
+                label: CONFIG['D35E'].currencies.cp,
+            },
         ];
+        game.settings.get('D35E', 'currencyConfig').currency.forEach(([id, label, weight, factor, group]) => {
+            currencies.push({ id, factor, label });
+        });
+        return currencies;
     }
     get configCanRollAbility() {
         return true;
     }
     get configLootItemType() {
-        return "loot";
+        return 'loot';
     }
     get itemPriceAttribute() {
-        return "system.price";
+        return 'system.price';
     }
     get itemQuantityAttribute() {
-        return "system.quantity";
+        return 'system.quantity';
     }
 }
--- module/D35E.ts
+++ module/D35E.ts
@@ -1,115 +1,153 @@
 export class D35E implements SystemApi {
+	get version() {
+		return 2;
+	}
 
-    get version() {
-        return 2;
-    }
+	get id() {
+		return 'D35E';
+	}
 
-    get id() {
-        return "D35E";
-    }
+	async actorRollSkill(actor, skillId): Promise<Roll | null> {
+		let roll = await actor.rollSkill(skillId);
+		return this.fixReadySetRoll(roll);
+	}
 
-    async actorRollSkill(actor, skillId):Promise<Roll|null> {
-        let roll = await actor.rollSkill(skillId);
-        return this.fixReadySetRoll(roll);
-    }
+	async actorRollAbility(actor, abilityId): Promise<Roll | null> {
+		let roll = await actor.rollAbilityTest(abilityId);
+		return this.fixReadySetRoll(roll);
+	}
 
-    async actorRollAbility(actor, abilityId): Promise<Roll|null> {
-        let roll =  await actor.rollAbilityTest(abilityId);
-        return this.fixReadySetRoll(roll);
-    }
+	fixReadySetRoll(roll) {
+		if (roll === null) {
+			return null;
+		}
+		if (roll.total === undefined) {
+			if (roll.fields !== undefined && roll.fields[2] !== undefined) {
+				roll = roll.fields[2][1]?.roll;
+			}
+		}
+		return roll;
+	}
 
-    async actorRollTool(actor, item): Promise<Roll|null> {
-        let roll = await item.rollToolCheck();
-        return this.fixReadySetRoll(roll);
-    }
+	actorCurrenciesGet(actor): Currencies {
+		function addCurrency(currency?: Currencies) {
+			if (currency) Object.entries(currency).forEach(([k, v]) => (currencies[k] = (currencies[k] ?? 0) + v));
+		}
+		const currencies: Currencies = {};
+		addCurrency(actor.system.currency);
+		addCurrency(actor.system.altCurrency);
+		addCurrency(actor.system.customCurrency);
+		return currencies;
+	}
 
-    fixReadySetRoll(roll){
-        if(roll === null){
-            return null
-        }
-        if(roll.total === undefined){
-            if(roll.fields !== undefined && roll.fields[2] !== undefined ){
-                roll =  roll.fields[2][1]?.roll
-            }
-        }
-        return roll;
-    }
+	async actorCurrenciesStore(actor, currencies: Currencies): Promise<void> {
+		await actor.update({ system: { currency: currencies } });
+	}
 
-    actorCurrenciesGet(actor):Currencies {
-        return actor["system"].currency;
-    }
+	actorSheetAddTab(sheet, html, actor, tabData: { id: string; label: string; html: string }, tabBody: string): void {
+		// Only applies to the standard Character Sheet
+		if (sheet.constructor.name !== 'Character') return;
 
-    async actorCurrenciesStore(actor, currencies: Currencies): Promise<void> {
-        await actor.update({system: {currency: currencies}});
-    }
+		const tabs = $(html).find('.tabs[data-group="primary"]');
+		const tabItem = $('<a class="item" data-tab="' + tabData.id + '">' + tabData.label + '</a>');
+		tabs.append(tabItem);
+		const body = $(html).find('.primary-body');
+		const tabContent = $('<div class="tab" data-group="primary" data-tab="' + tabData.id + '"></div>');
+		body.append(tabContent);
+		tabContent.append(tabBody);
 
-    actorSheetAddTab(sheet, html, actor, tabData:{ id: string, label: string, html: string }, tabBody:string): void {
-        const tabs = $(html).find('.tabs[data-group="primary"]');
-        const tabItem = $('<a class="item" data-tab="' + tabData.id + '">' + tabData.label + '</a>');
-        tabs.append(tabItem);
-        const body = $(html).find(".sheet-body");
-        const tabContent = $('<div class="tab" data-group="primary" data-tab="' + tabData.id + '"></div>');
-        body.append(tabContent);
-        tabContent.append(tabBody);
-    }
+		// Expands the sheet
+		if (sheet.position.width < 836) {
+			sheet.position.width = 836;
+			sheet.element[0].style.width = '836px';
+		}
+	}
 
-    get configSkills(): SkillConfig[] {
-        return Object.entries(game["D35E"].config.skills)
-            .map(skills => {
-                // @ts-ignore
-                return {id: skills[0], label: skills[1].label as string};
-            })
-    }
+	itemSheetReplaceContent(app, html, element): void {
+		html.find('.summary')[0].innerHTML = '<li>Recipe</li>';
+		if (game.user!.isGM === false && app.object.system.identified === false) return;
 
-    get configAbilities(): AbilityConfig[] {
-        return Object.entries(game["D35E"].config.abilities).map(ab => {
-            return {id: ab[0], label: ab[1] as string};
-        });
-    }
+		const sheetNavigation = html.find('.sheet-navigation[data-group=primary]');
+		const initialTab = app._initialTab.primary === 'crafting';
+		sheetNavigation.append($(`<a class="item ${initialTab ? 'active' : ''}" data-tab="crafting">Crafting</a>`));
 
-    get configCurrencies(): CurrencyConfig[] {
-        return [
-            {
-                id: "pp",
-                factor: 1000,
-                label: game["D35E"].config.currencies.pp.label
-            },
-            {
-                id: "gp",
-                factor: 100,
-                label: game["D35E"].config.currencies.gp.label
-            },
-            {
-                id: "ep",
-                factor: 50,
-                label: game["D35E"].config.currencies.ep.label
-            },
-            {
-                id: "sp",
-                factor: 10,
-                label: game["D35E"].config.currencies.sp.label
-            },
-            {
-                id: "cp",
-                factor: 1,
-                label: game["D35E"].config.currencies.cp.label
-            }
-        ]
-    }
+		const body = $(`<div class="tab crafting ${initialTab ? 'active' : ''}" data-group="primary" data-tab="crafting"></div>`);
+		body.append(element);
+		html.find('.primary-body').append(body);
+	}
 
-    get configCanRollAbility():boolean {
-        return true;
-    }
-    get configLootItemType(): string {
-        return "loot";
-    }
+	get configSkills(): SkillConfig[] {
+		function includeSubSkills(actor) {
+			arbitrarySkills.forEach((id) => {
+				const skill = actor.system.skills[id];
+				if (skill === undefined) return;
+				Object.entries(skill.namedSubSkills).forEach(([k, v]) => {
+					const subSkillId = `${id}.namedSubSkills.${k}`;
+					if (subSkillId in skills) return;
+					const label = `${CONFIG['D35E'].skills[id]}: ${(v as { name: string }).name}`;
+					skills[subSkillId] = { id: subSkillId, label };
+				});
+			});
+		}
+		const arbitrarySkills = CONFIG['D35E'].arbitrarySkills;
+		const skills: Record<string, { id: string; label: string }> = {};
+		Object.entries(CONFIG['D35E'].skills).forEach(([id, label]: [string, string]) => {
+			if (arbitrarySkills.includes(id) === false) skills[id] = { id, label };
+		});
+		game.actors!.forEach(includeSubSkills);
+		return Object.values(skills).sort((a, b) => a.label.localeCompare(b.label));
+	}
 
-    get itemPriceAttribute(): string {
-        return "system.price";
-    }
+	get configAbilities(): AbilityConfig[] {
+		return Object.entries(CONFIG['D35E'].abilities).map((ab) => {
+			return { id: ab[0], label: ab[1] as string };
+		});
+	}
 
-    get itemQuantityAttribute(): string {
-        return "system.quantity";
-    }
+	get configCurrencies(): CurrencyConfig[] {
+		const currencies = [
+			{
+				id: 'pp',
+				factor: 1000,
+				label: CONFIG['D35E'].currencies.pp,
+			},
+			{
+				id: 'gp',
+				factor: 100,
+				label: CONFIG['D35E'].currencies.gp,
+			},
+			{
+				id: 'sp',
+				factor: 10,
+				label: CONFIG['D35E'].currencies.sp,
+			},
+			{
+				id: 'cp',
+				factor: 1,
+				label: CONFIG['D35E'].currencies.cp,
+			},
+		];
+		(
+			game.settings.get('D35E', 'currencyConfig') as { currency: [string, string, number, number, string][] }
+		).currency.forEach(([id, label, weight, factor, group]) => {
+			currencies.push({ id, factor, label });
+		});
+		return currencies;
+	}
 
+	get configCanRollAbility(): boolean {
+		return true;
+	}
+
+	get configLootItemType(): string {
+		return 'loot';
+	}
+
+	get itemPriceAttribute(): string {
+		return 'system.price';
+	}
+
+	get itemQuantityAttribute(): string {
+		return 'system.quantity';
+	}
 }
